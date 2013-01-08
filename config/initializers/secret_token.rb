58f9ccc0126fd0971dd824c306561ad691b40f5e
nikunj.0407@gmail.com <nikunj.0407@gmail.com>
2013-01-08 16:13:55 +0530

    Dummy Blog Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummyblog::Application.config.secret_token = '558b7126ea611f2f5f43fc74f3e6910da90304f1f0e461b638d5f1ce5d8ffef1185a33783291d6dff271ebabc544b648a1b73023c4bde14195570e153fee819d'
